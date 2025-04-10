a799ac64506ebf01b32469b761b7f8da1d3f03c0
mikailkraft <94494913+mikailkraft@users.noreply.github.com>
2025-04-10 14:00:19 -0600

    Update CustomImage.js

--- components/CustomImage.js
+++ components/CustomImage.js
@@ -4,7 +4,7 @@ export default function CustomImage({ src, alt, ...otherProps }) {
   return (
     <figure className="relative w-full h-auto">
       <Image
-        className="object-cover object-top"
+        className="object-cover object-top w-full h-auto"
         src={src}
         alt={alt || ''}
         fill={true}
