739e06e17bb30f014e8755f35c6d6264076b81ec
mikailkraft <94494913+mikailkraft@users.noreply.github.com>
2025-04-16 14:12:10 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,14 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,326;1,326&family=Rubik+Mono+One&display=swap" rel="stylesheet">
       <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">{name}</Link>
       </p>
+    
     </header>
   );
 }
